6377c9b880577f51d0e261a367bf1a792e9f4645
Minho Park <iampmh0601@gmail.com>
2020-01-30 22:19:13 +0900

    ReactDOM can render only one component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Minho from './minho';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!!!</h1>
+      <Minho />
+      <Minho />
+      <Minho />
+      <Minho />
     </div>
   );
 }
---
+++ src/minho.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Minho(){
+    return <h3>I love me</h3>
+}
+
+export default Minho;
